920b491d81dd31b1ff773e47d80ab5431fa134c0
PrajwalM20 <prajwalm1122@gmail.com>
2025-11-11 16:34:03 +0530

    second commit

--- trainers_search_app/trainers/models.py
+++ trainers_search_app/trainers/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Trainer(models,model):
+class Trainer(models.Model):
     name=models.CharField(max_length=50)
     place=models.CharField(max_length=50)
     email=models.EmailField()
